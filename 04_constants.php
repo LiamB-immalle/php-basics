dc89298946a5d114e81698e29c9defbda77bccd3
liamb-immalle <liam.brangers@immalle.be>
2018-01-09 14:46:25 +0100

    Oef 5

--- 04_constants.php
+++ 04_constants.php
@@ -1,8 +1,12 @@
 <?php
 
 define("USERNAME", "joske");
+define("PASSWORD", "ske");
+define("SERVERNAME", "oske");
 
 echo USERNAME;
+echo SERVERNAME;
+echo PASSWORD;
 
 // definieer ook een constante PASSWORD en SERVERNAME
 
