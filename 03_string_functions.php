2b7ca2b01aa8be3826e9b4386c01df51f2cf6c22
liamb-immalle <liam.brangers@immalle.be>
2018-01-09 14:30:37 +0100

    Oef 4

--- 03_string_functions.php
+++ 03_string_functions.php
@@ -4,8 +4,9 @@ $zin = "Binnenkort is het weeral lente";
 
 // Print af hoeveel woorden $zin bevat.
 
-
+echo str_word_count($zin);
 // Vervang in $zin "lente" door "zomer" en druk af.
 
+echo str_replace(lente,zomer,$zin);
 
 ?>
